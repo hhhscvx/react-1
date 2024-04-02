987763176eaa5e7b96a6b617b5d06df8eddfd4db
hhhscvx <hhhscvx@yandex.ru>
2024-04-02 07:42:35 +0500

    can add new items

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,34 @@ import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
 import JournalForm from "./components/JournalForm/JournalForm";
 import Body from "./layouts/Body/Body";
 import LeftPanel from "./layouts/leftPanel/LeftPanel";
+import { useState } from "react";
 
+const INITIAL_DATA = [
+  {
+    title: "Подготовка к обновлению курсов",
+    text: "Горные походы открывают удивительные природные ландшафт",
+    date: new Date(),
+  },
+  {
+    title: "Поход в годы",
+    text: "Думал, что очень много времени",
+    date: new Date(),
+  },
+];
 
 function App() {
-  const data = [
-    {
-      title: "Первая заметка",
-      date: new Date(),
-      text: "(coca)",
-    },
-    {
-      title: "анжумания",
-      date: new Date(),
-      text: "Тудулистu",
-    },
-  ];
+  const [items, setItems] = useState(INITIAL_DATA);
+
+  const addItem = (item) => {
+    setItems((oldItems) => [
+      ...oldItems, // создаем новый массив со всеми старыми items + новым item
+      {
+        text: item.text,
+        title: item.title,
+        date: new Date(item.date)
+      },
+    ]);
+  };
 
   return (
     <div className="app">
@@ -32,27 +45,17 @@ function App() {
         <JournalAddButton></JournalAddButton>
 
         <JournalList>
-          <CardButton>
-            <JournalItem
-              title={data[0].title}
-              text={data[0].text}
-              date={data[0].date}
-            />
-          </CardButton>
-
-          <CardButton>
-            <JournalItem
-              title={data[1].title}
-              text={data[1].text}
-              date={data[1].date}
-            />
-          </CardButton>
+          {/* {[<Button>1</Button>, <Button>2</Button>]} */}
+          {items.map((el) => (
+            <CardButton>
+              <JournalItem title={el.title} text={el.text} date={el.date} />
+            </CardButton>
+          ))}
         </JournalList>
       </LeftPanel>
       <Body>
-        <JournalForm>
-
-        </JournalForm>
+        <JournalForm onSubmit={addItem}></JournalForm>{" "}
+        {/* Передаем функцию addItem как props в JournalForm */}
       </Body>
     </div>
   );
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,31 +1,25 @@
 import Button from "../Button/Button";
 import "./JournalForm.css";
-import { useState } from "react";
 
-function JournalForm() {
-  const [inputData, setInputData] = useState("");
 
-  const inputChange = (event) => {
-    setInputData(event.target.value);
-    console.log(inputData);
-  };
-
+function JournalForm({ onSubmit }) {
   const addJournalItem = (event) => {
+    event.preventDefault();
+
     const formData = new FormData(event.target);
     const formProps = Object.fromEntries(formData);
 
-    event.preventDefault();
-    console.log(formProps);
+    onSubmit(formProps); // вызываем добавление нового объекта в items
   };
 
   return (
     <form className="journal-form" onSubmit={addJournalItem}>
-      <input type="text" name="title" />
 
+      <input type="text" name="title" />
       <input type="date" name="date" />
-      <input type="text" name="tag" value={inputData} onChange={inputChange} />
+      <input type="text" name="tag" />
       <textarea name="post" id="" cols="30" rows="10"></textarea>
-      <Button text="Сохранить" />
+      <Button text="Сохранить" onClick={() => {console.log('Clicked')}}/>
     </form>
   );
 }
